50c3b8bf10fe49f271f19f9499ad78d48d691d3c
Trent Rand <trand@squarespace.com>
2019-09-18 16:44:55 -0400

    Implement core logic of the yank command

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import yargs from 'yargs';
+import { GitProcess, GitError, IGitResult } from 'dugite';
+import generateBranchName from 'project-name-generator';
+
+const pathToRepository = '.'
 
 const argv = yargs.usage(
   `Move a commit or series of commits to a new branch
@@ -58,8 +62,110 @@ let args = {
 };
 
 (async () => {
+  const sourceBranchName = await getCurrentGitBranchName();
+
   if (args.debug) {
     console.log(`Source Branch Name: ${sourceBranchName}`);
     console.log(`Options: ${JSON.stringify(args, null, 2)}`);
   }
+
+  if (args.destinationBranchName === undefined) {
+    args.destinationBranchName = generateBranchName({ words: 2, alliterative: true }).dashed;
+  }
+
+  // Create new branch or checkout existing
+  await createBranchIfDoesNotExist(args.destinationBranchName, args.startPoint);
+
+  // Switch to the specified destination branch
+  await checkoutBranch(args.destinationBranchName);
+
+  // Cherry pick the specified commits
+  await cherryPickCommits(args.commits);
+
+  // Conditionally push the destination branch
+  if (args.push) {
+    await pushCurrentBranch();
+  }
+
+  // Checkout previous branch
+  await checkoutBranch(sourceBranchName);
+
+  // Conditionally remove the specified commits from the source branch
+  if (!args.safe) {
+    for (let commitIdentifier of args.commits) {
+      await removeCommitFromBranch(commitIdentifier, sourceBranchName);
+    }
+  }
+
+  console.log(`Successfully yanked the specified commits to a branch named ${args.destinationBranchName}.`);
 })();
+
+async function createBranchIfDoesNotExist(branchName, startPoint) {
+  const command = `branch ${branchName} ${startPoint}`.split(' ');
+  const { stdout, stderr, exitCode } = await GitProcess.exec(command, pathToRepository);
+
+  if (exitCode !== 0) {
+    const parsedError = GitProcess.parseError(stderr);
+    if (parsedError) {
+      if (parsedError === GitError.BranchAlreadyExists) {
+        console.info(`Moving commits to a branch named '${branchName}'.`);
+      } else {
+        Logger.error(stderr, exitCode);
+      }
+    } else {
+      console.info(`Created a branch named '${branchName}'. Commits will be moved to that branch.`);
+    }
+  }
+}
+
+async function checkoutBranch(branchName) {
+  const command = `checkout ${branchName}`.split(' ');
+  const { stdout, stderr, exitCode } = await GitProcess.exec(command, pathToRepository);
+
+  if (exitCode !== 0) {
+    Logger.error(stderr, exitCode);
+  }
+}
+
+async function cherryPickCommits(commits) {
+  const commitsList = commits.join(' ');
+  const { stdout, stderr, exitCode } = await GitProcess.exec(['cherry-pick', commitsList], pathToRepository);
+
+  if (exitCode !== 0) {
+    Logger.error(stderr, exitCode);
+  }
+}
+
+async function pushCurrentBranch() {
+  const { stdout, stderr, exitCode } = await GitProcess.exec(['push'], pathToRepository);
+
+  if (exitCode !== 0) {
+    Logger.error(stderr, exitCode);
+  }
+}
+
+async function getCurrentGitBranchName() {
+  const command = `rev-parse --abbrev-ref HEAD`.split(' ');
+  const { stdout: branchName, stderr, exitCode } = await GitProcess.exec(command, pathToRepository);
+
+  if (exitCode !== 0) {
+    Logger.error(stderr, exitCode);
+  }
+
+  return branchName.trim();
+}
+
+async function removeCommitFromBranch(commitIdentifier, sourceBranchName) {
+  const command = `rebase -p --onto ${commitIdentifier}~ ${commitIdentifier} ${sourceBranchName}`.split(' ');
+  const { stdout, stderr, exitCode } = await GitProcess.exec(command, pathToRepository);
+
+  if (exitCode !== 0) {
+    Logger.error(stderr, exitCode);
+  }
+}
+
+const Logger = {
+  error: (error, exitCode) => {
+    console.error(`Error ${exitCode}: '${error}'`);
+  }
+}
